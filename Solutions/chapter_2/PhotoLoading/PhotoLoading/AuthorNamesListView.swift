6525aeac6bdd2829d4df1bf99ce7cba18c0f4028
Pere Bohigas <pere.bohigas@gmail.com>
2024-02-16 17:37:51 +0100

    Add asynchronous loading of Photo objects

--- Solutions/chapter_2/PhotoLoading/PhotoLoading/AuthorNamesListView.swift
+++ Solutions/chapter_2/PhotoLoading/PhotoLoading/AuthorNamesListView.swift
@@ -1,20 +1,36 @@
 import SwiftUI
 
 struct AuthorNamesListView: View {
+    @Environment(\.remoteURL) var remoteURL: URL?
+
     @State var photos: [Photo]?
 
     var body: some View {
-        if let photos {
-            ScrollView {
-                ForEach(photos) { photo in
-                    Label(photo.author, systemImage: "person.crop.circle")
-                        .frame(maxWidth: .infinity, alignment: .leading)
+        Group {
+            if let photos {
+                ScrollView {
+                    ForEach(photos) { photo in
+                        Label(photo.author, systemImage: "person.crop.circle")
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
                 }
+                .padding()
+            } else {
+                ProgressView("Loading")
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
             }
-            .padding()
-        } else {
-            ProgressView("Loading")
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        .task {
+            guard let remoteURL else { return }
+
+            let remote: Remote<[Photo]> = .init(url: remoteURL)
+
+            guard let photos: [Photo] = await remote.load() else {
+                self.photos = []
+                return
+            }
+
+            self.photos = photos
         }
     }
 }
